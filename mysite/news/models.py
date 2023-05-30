a79eec5892f7bb3a400b059755b38877cb067c17
KseniaDanilowa <ksenija.dan1lova@yandex.ru>
2023-05-30 21:56:56 +0300

    .

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Articles(models.Model):
+    title = models.CharField('Название', max_length=250)
+    content = models.TextField('Статья')
+    date = models.DateTimeField('Дата публикации')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+
+
